8adfb5ec4e3eed61909aa19984a078d764cd6b26
gotanda555 <crok2558@gmail.com>
2022-12-17 13:01:49 +0900

    mod: timeof

--- src/App.tsx
+++ src/App.tsx
@@ -59,6 +59,14 @@ company = "Amazon";
 let memory: 256 | 512;
 memory = 256;
 
+// timeof
+let msg: string = "Hi";
+let msg2: typeof msg;
+msg2 = "hello";
+
+let animal = {cat: "small cat"};
+let newAnimal: typeof animal = {cat: "big cat"}
+
 
 function App() {
   return (
